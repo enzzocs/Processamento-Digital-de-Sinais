36d45e996beb9474c0c399779791e380da4d8133
enzzocs <csenzzo@gmail.com>
2023-01-04 14:53:07 -0300

    Add files via upload

---
+++ Enzzo - Rejeita Faixas/stm32f4_discovery_wolfson_pi_filtro_IIR_BIQUAD/Debug/system/src/cmsis/vectors_stm32f407xx.d
@@ -0,0 +1,4 @@
+system/src/cmsis/vectors_stm32f407xx.o: \
+ ../system/src/cmsis/vectors_stm32f407xx.c \
+ ../system/include/cortexm/ExceptionHandlers.h
+../system/include/cortexm/ExceptionHandlers.h:
---
+++ Enzzo - Rejeita Faixas/stm32f4_discovery_wolfson_pi_filtro_IIR_BIQUAD/Debug/system/src/stm32f4-hal/subdir.mk
@@ -0,0 +1,60 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../system/src/stm32f4-hal/stm32f4xx_hal.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_cortex.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_dfsdm.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_dma.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_flash.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_gpio.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_i2s.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_i2s_ex.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_iwdg.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_pwr.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_rcc.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_rcc_ex.c \
+../system/src/stm32f4-hal/stm32f4xx_hal_spi.c 
+
+OBJS += \
+./system/src/stm32f4-hal/stm32f4xx_hal.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_cortex.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_dfsdm.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_dma.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_flash.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_gpio.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_i2s.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_i2s_ex.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_iwdg.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_pwr.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_rcc.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_rcc_ex.o \
+./system/src/stm32f4-hal/stm32f4xx_hal_spi.o 
+
+C_DEPS += \
+./system/src/stm32f4-hal/stm32f4xx_hal.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_cortex.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_dfsdm.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_dma.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_flash.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_gpio.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_i2s.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_i2s_ex.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_iwdg.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_pwr.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_rcc.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_rcc_ex.d \
+./system/src/stm32f4-hal/stm32f4xx_hal_spi.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+system/src/stm32f4-hal/%.o: ../system/src/stm32f4-hal/%.c system/src/stm32f4-hal/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU ARM Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DARM_MATH_CM4 -DOS_USE_SEMIHOSTING -DUSE_FULL_ASSERT -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"../system/src/Components" -I"../system/src/stm32f4_discovery" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -Wno-bad-function-cast -Wno-conversion -Wno-sign-conversion -Wno-unused-parameter -Wno-sign-compare -Wno-missing-prototypes -Wno-missing-declarations -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
