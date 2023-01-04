2e379b830e37d5f619ae0997e61d7770bb695425
enzzocs <csenzzo@gmail.com>
2023-01-04 14:56:51 -0300

    Add files via upload

---
+++ Enzzo - Rejeita Faixas/stm32f4_discovery_wolfson_pi_filtro_fir/Debug/src/subdir.mk
@@ -0,0 +1,55 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/_initialize_hardware.c \
+../src/_write.c \
+../src/dwt.c \
+../src/main.c \
+../src/stm32f4xx_hal_msp.c \
+../src/stm32f4xx_it.c \
+../src/tests.c \
+../src/utils.c \
+../src/wolfson_pi_audio.c 
+
+OBJS += \
+./src/_initialize_hardware.o \
+./src/_write.o \
+./src/dwt.o \
+./src/main.o \
+./src/stm32f4xx_hal_msp.o \
+./src/stm32f4xx_it.o \
+./src/tests.o \
+./src/utils.o \
+./src/wolfson_pi_audio.o 
+
+C_DEPS += \
+./src/_initialize_hardware.d \
+./src/_write.d \
+./src/dwt.d \
+./src/main.d \
+./src/stm32f4xx_hal_msp.d \
+./src/stm32f4xx_it.d \
+./src/tests.d \
+./src/utils.d \
+./src/wolfson_pi_audio.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU ARM Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DARM_MATH_CM4 -DOS_USE_SEMIHOSTING -DUSE_FULL_ASSERT -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"../system/src/Components" -I"../system/src/stm32f4_discovery" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+src/stm32f4xx_hal_msp.o: ../src/stm32f4xx_hal_msp.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU ARM Cross C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -fno-move-loop-invariants -Wall -Wextra  -g3 -DDEBUG -DARM_MATH_CM4 -DOS_USE_SEMIHOSTING -DUSE_FULL_ASSERT -DTRACE -DOS_USE_TRACE_SEMIHOSTING_DEBUG -DSTM32F407xx -DUSE_HAL_DRIVER -DHSE_VALUE=8000000 -I"../include" -I"../system/src/Components" -I"../system/src/stm32f4_discovery" -I"../system/include" -I"../system/include/cmsis" -I"../system/include/stm32f4-hal" -std=gnu11 -Wno-missing-prototypes -Wno-missing-declarations -MMD -MP -MF"$(@:%.o=%.d)" -MT"src/stm32f4xx_hal_msp.d" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Enzzo - Rejeita Faixas/stm32f4_discovery_wolfson_pi_filtro_fir/Debug/src/utils.d
@@ -0,0 +1,9 @@
+src/utils.o: ../src/utils.c ../include/utils.h \
+ ../system/include/cmsis/arm_math.h \
+ ../system/include/cmsis/cmsis_compiler.h \
+ ../system/include/cmsis/cmsis_gcc.h ../system/include/diag/Trace.h
+../include/utils.h:
+../system/include/cmsis/arm_math.h:
+../system/include/cmsis/cmsis_compiler.h:
+../system/include/cmsis/cmsis_gcc.h:
+../system/include/diag/Trace.h:
